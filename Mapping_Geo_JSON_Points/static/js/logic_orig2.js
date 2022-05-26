e7e072c629b491fb2582f49118a1b477555087b6
Frank <frw2397@aol.com>
2022-05-25 23:52:21 -0500

    updated

--- Mapping_Geo_JSON_Points/static/js/logic_orig2.js
+++ Mapping_Geo_JSON_Points/static/js/logic_orig2.js
@@ -1,9 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-
 // Create the map object with center and zoom level.
-//let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([30, 30], 2);
 
 // Add GeoJSON data.
 //let sanFranAirport =
@@ -53,28 +52,6 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/t
     accessToken: API_KEY
 });
 
-
-// We create the tile layer that will be the background of our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
-
-// Create a base layer that holds both maps.
-let baseMaps = {
-  Street: streets,
-  Dark: dark
-};
-
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-  center: [30, 30],
-  zoom: 2,
-  layers: [streets]
-});
-
-
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/fwhite2397/Mapping_Earthquakes/main/Mapping_Geo_JSON_Points/Resources/majorAirports.json";
 
@@ -92,5 +69,5 @@ L.geoJSON(data, {
 }).addTo(map);
 });
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
